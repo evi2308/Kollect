3a19152fbddf85248f1ee93e6a83c93836dec9f0
Eva <evasanchezm23@gmail.com>
2023-10-24 22:49:17 +0200

    feat: Añadido AppNavigation y AppScreens feat:Creada la navegacion entre Login y PantallaPrincipal LoginScreen definida como pantalla de inicio feat: Añadido Register, se registran los usuarios y ya no se puede echar atrás una vez iniciada sesion feat: Añadidos colores a theme y color feat:No se puede echar atras en registro a login feat:cambios y empiezo la pantalla principal feat: Se guardan username y email en firestore

---
+++ app/src/main/java/com/evasanchez/kollect/data/Usuario.kt
@@ -0,0 +1,64 @@
+package com.evasanchez.kollect.data
+
+data class Usuario(
+    val userId: String = "",
+    val username: String = "",
+    val email: String = "",
+    val pfpURL: String = "",
+    val contPcs: String = "",
+    val valuePcs: String = "",
+    val wishlist: List<Wishlist> = emptyList(),
+    val coleccion: List<Coleccion> = emptyList(),
+    val kgroups: List<Kgroups> = emptyList(),
+    val idols: List<Idols> = emptyList()
+) {
+    fun userToMap(): MutableMap<String, Any>{
+        return mutableMapOf(
+            "user_id" to this.userId,
+            "username" to this.username,
+            "email" to this.email,
+            "pfpURL" to this.pfpURL,
+            "cont_pcs" to this.contPcs,
+            "valuePcs" to this.valuePcs,
+            "wishlist" to this.wishlist,
+            "coleccion" to this.coleccion,
+            "kgroups" to this.kgroups,
+            "idols" to this.idols
+        )
+    }
+}
+
+data class Kgroups(
+    val groupId: String,
+    val groupName: String
+)
+
+data class Idols(
+    val idolId: String,
+    val idolName: String
+)
+
+data class Wishlist(
+    val photocardId: String,
+    val albumName: String,
+    val status: String,
+    val groupName: String,
+    val idolName: String,
+    val value: String,
+    val type: String,
+    val photocardURL: String,
+    val photocardVersion: String
+)
+
+data class Coleccion(
+    val photocardId: String,
+    val albumName: String,
+    val status: String,
+    val groupName: String,
+    val idolName: String,
+    val value: String,
+    val type: String,
+    val photocardURL: String,
+    val photocardVersion: String
+)
+
